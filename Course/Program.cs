d8fe25fda5f98f8f967b83a0132102aa2fd83b69
Igor castro <igor.castro@xbits.com.br>
2022-09-04 16:07:34 -0300

    ICA [course] first progam

---
+++ Course/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace Course
+{
+    class MyCourse
+    {
+        static void Main()
+        { 
+            var firstNumber = 255;
+            var secondNumber = 2311212312;
+            firstNumber++;
+            var isComplete = false;
+            var geneder = 'F';
+            string name; //null
+            name = "Igor";
+            Console.WriteLine(name);
+            var newFloatValue = 123.231;
+            Console.WriteLine(newFloatValue);
+        }
+
+    }
+}
--- primeiro programa/Program.cs
+++ primeiro programa/Program.cs
@@ -3,7 +3,7 @@ namespace PrimeiroPrograma
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             Console.WriteLine("Primeira Classe");
         }
