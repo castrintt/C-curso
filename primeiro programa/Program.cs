fb3778b52f7b24db99c5f1aeeab58c292fab105f
Igor castro <igor.castro@xbits.com.br>
2022-09-04 14:07:36 -0300

    ICA [Course] first commit

---
+++ primeiro programa/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace PrimeiroPrograma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Primeira Classe");
+        }
+    }
+}
